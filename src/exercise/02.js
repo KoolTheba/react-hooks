89efebc15180fffd04bdbd202c35804ba6988df4
KoolTheba <gomez.teba@gmail.com>
2023-12-22 12:01:44 +0100

    feat: completed exercise 2 with extra features for localstorage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,18 +1,42 @@
 // useEffect: persistent state
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
+const useLocalStorageState = (key, defaultValue = '', {
+  serialize = JSON.stringify,
+  deserialize = JSON.parse,
+} = {}) => {
+  const [nameInStorage, setNameInStorage] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+      if(valueInLocalStorage){
+        return deserialize(valueInLocalStorage)
+      }
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+  })
 
-  // The callback should set the `name` in localStorage.
+  // controlls changes in key, keeping previous reference
+  const prevKeyRef = React.useRef(key);
+
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  },[name])
+    const prevKey = prevKeyRef.current;
+    if(prevKey !== key){
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(nameInStorage))
+  }, [key, nameInStorage, serialize])
+  
+  return [
+    nameInStorage,
+    setNameInStorage
+  ]
+}
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
+function Greeting({initialName = ''}) {
+  // use the custom hook
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  const handleChange = (event) => setName(event.target.value)
+
   return (
     <div>
       <form>
